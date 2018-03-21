818fc206e3cdab79cc15e3a788647d602be01b22
Ariana Berger <ariana.berger@gmail.com>
2018-03-21 17:52:19 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -13,17 +13,14 @@ class Student
   end
 
   def self.create_table
-    sql = <<~ SQL
+    sql = <<-SQL
       CREATE TABLE IF NOT EXISTS songs (
       id INTEGER PRIMARY KEY,
       name TEXT,
       grade TEXT
       )
       SQL
-
       DB[:conn].execute(sql)
     end
-
-  end
 
 end
